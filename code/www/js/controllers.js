7ff26ada32c043cebaa682309333dce3333457dd
sked1985 <mcgettiganbarry@yahoo.com>
2015-03-13 11:58:03 +0000

    Mobile Project
    This is my project for my mobile application module. It`s an app that
    works like tinder only with music. The app plays music by random artists
    to the user for 30 seconds and they can choose whether they want to
    favorite that artist or skip.

---
+++ code/www/js/controllers.js
@@ -0,0 +1,54 @@
+angular.module('songhop.controllers', ['ionic', 'songhop.services'])
+
+
+/*
+Controller for the discover page
+*/
+    .controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
+
+      Recommendations.getNextSongs()
+   .then(function(){
+     $scope.currentSong = Recommendations.queue[0];
+   });
+
+
+
+      $scope.sendFeedback = function(bool){
+
+      if (bool) User.addSongToFavorites($scope.currentSong);
+
+        $scope.currentSong.rated = bool;
+        $scope.currentSong.hide = true;
+
+        $scope.removeSong = function(song, index) {
+            User.removeSongFromFavorites(song, index);
+          }
+
+          Recommendations.nextSong();
+
+        $timeout(function() {
+          // $timeout to allow animation to complete
+          $scope.currentSong = Recommendations.queue[0];
+        }, 250);
+
+
+
+    }
+  })
+
+
+/*
+Controller for the favorites page
+*/
+    .controller('FavoritesCtrl', function($scope, User) {
+
+        $scope.favorites = User.favorites;
+      })
+
+
+/*
+Controller for our tab bar
+*/
+    .controller('TabsCtrl', function($scope) {
+
+      });
