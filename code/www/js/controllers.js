74a0f4903a172c374a56ee8ab9cf7be605af5b1f
sked1985 <mcgettiganbarry@yahoo.com>
2015-03-14 16:04:07 +0000

    Core functionality
    Core functionality added. The app now places music for thirty seconds
    and the user can favorite the songs they like and then go to spotify to
    view their songs.

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,11 +4,27 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-    .controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
+    .controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
 
-      Recommendations.getNextSongs()
-   .then(function(){
-     $scope.currentSong = Recommendations.queue[0];
+      var showLoading = function() {
+          $ionicLoading.show({
+            template: '<i class="ion-loading-c"></i>',
+            noBackdrop: true
+          });
+        }
+
+        var hideLoading = function() {
+            $ionicLoading.hide();
+        }
+
+      showLoading();
+
+      Recommendations.init()
+        .then(function(){
+          $scope.currentSong = Recommendations.queue[0];
+          Recommendations.playCurrentSong();
+          hideLoading();
+
    });
 
 
@@ -30,6 +46,8 @@ Controller for the discover page
           // $timeout to allow animation to complete
           $scope.currentSong = Recommendations.queue[0];
         }, 250);
+
+        Recommendations.playCurrentSong();
     }
 
     $scope.nextAlbumImg = function(){
@@ -44,15 +62,30 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-    .controller('FavoritesCtrl', function($scope, User) {
+    .controller('FavoritesCtrl', function($scope, $window, User) {
 
+        $scope.openSong = function(song){
+          $window.open(song.open_url, "system");
+        }
         $scope.favorites = User.favorites;
       })
+
+
 
 
 /*
 Controller for our tab bar
 */
-    .controller('TabsCtrl', function($scope) {
+    .controller('TabsCtrl', function($scope, User, Recommendations) {
+
+      $scope.favCount = User.favoriteCount;
+
+      $scope.enteringFavorites = function(){
+        User.newFavorites = 0;
+        Recommendations.haltAudio();
+      }
+      $scope.leavingFavorites = function(){
+        Recommendations.init();
+      }
 
       });
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -3,7 +3,8 @@ angular.module('songhop.services', [])
   .factory('User', function(){
 
     var o ={
-      favorites: []
+      favorites: [],
+      newFavorites: 0
     }
 
     o.addSongToFavorites = function(song){
@@ -11,6 +12,11 @@ angular.module('songhop.services', [])
       if(!song) return false;
 
       o.favorites.unshift(song);
+      o.newFavorites++;
+    }
+
+    o.favoriteCount = function(){
+      return o.newFavorites;
     }
 
     o.removeSongFromFavorites = function(song, index){
@@ -24,10 +30,37 @@ angular.module('songhop.services', [])
 
   })
 
-  .factory('Recommendations', function($http, SERVER){
+  .factory('Recommendations', function($http, SERVER, $q){
+    var media;
     var o ={
       queue: []
     };
+
+    o.playCurrentSong = function(){
+      var defer = $q.defer;
+
+      media = new Audio(o.queue[0].preview_url);
+
+      media.addEventListener("loadeddata", function(){
+        defer.resolve();
+      });
+
+      media.play();
+
+      return defer.promise;
+    }
+
+    o.haltAudio = function(){
+      if(media)media.pause();
+    }
+
+    o.init = function(){
+      if(o.queue.length ===0){
+        return o.getNextSongs();
+      }else{
+        return o.playCurrentSong();
+      }
+    }
 
     o.getNextSongs = function() {
      return $http({
@@ -42,6 +75,8 @@ angular.module('songhop.services', [])
    o.nextSong = function() {
     // pop the index 0 off
     o.queue.shift();
+
+    o.haltAudio();
 
     // low on the queue? lets fill it up
     if (o.queue.length <= 3) {
